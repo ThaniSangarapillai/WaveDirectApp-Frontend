21bb345b18c0a639955c815b92b4bbc217167ec7
RansikaP <ransika.p27@gmail.com>
2020-09-26 17:49:36 -0400

    header

--- WaveDirectApp/components/Splash.js
+++ WaveDirectApp/components/Splash.js
@@ -1,4 +1,4 @@
-import React, {useReducer, Component} from 'react';
+import React, {useReducer, Component, useState} from 'react';
 import {
   View,
   Text,
@@ -6,12 +6,20 @@ import {
   ScrollView,
   TextInput,
   useWindowDimensions,
+  StyleSheet,
 } from 'react-native';
+import {Header} from 'react-native-elements';
 import logo from './logo.png';
-import {Header} from 'react-native/Libraries/NewAppScreen';
 
 function Splash() {
   const window = useWindowDimensions();
+  // const [drawer, setDrawer] = useState(False);
+  const [title, setTitle] = useState('thani');
+
+  const clickHandler = () => {
+    setTitle = 'pira';
+  };
+
   return (
     <>
       {/* <Image
@@ -19,9 +27,21 @@ function Splash() {
         style={{width: window.width / 1.2, top: window.height / 2}}
         resizeMode="contain"
       /> */}
-      <Header></Header>
+      <Header
+        leftComponent={{icon: 'menu', color: '#fff'}}
+        onPress={clickHandler}
+        centerComponent={{text: title, style: {color: '#fff'}}}
+        rightComponent={{icon: 'home', color: '#fff'}}
+        containerStyle={styles.header}
+      />
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#FF0000',
+  },
+});
 
 export default Splash;
