d81ada0fa310338808490e1479b3eef23f87e3f4
RansikaP <ransika.p27@gmail.com>
2020-09-26 22:48:26 -0400

    Update Home.js

--- WaveDirectApp/components/Home.js
+++ WaveDirectApp/components/Home.js
@@ -12,60 +12,19 @@ function Home() {
     setTitle('pira');
   };
 
-  //   return (
-  //     <>
-  //       <Header
-  //         leftComponent={{
-  //           icon: 'menu',
-  //           color: '#fff',
-  //           onPress: () => clickHandler(),
-  //         }}
-  //         centerComponent={{text: title, style: {color: '#fff'}}}
-  //         rightComponent={{icon: 'home', color: '#fff'}}
-  //       />
-  //       <View style={styles.container}></View>
-  //     </>
-
-  //   );
-
-  function HomeScreen({navigation}) {
-    return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Button
-          onPress={() => navigation.navigate('Notifications')}
-          title="Go to notifications"
+   return (
+      <>
+        <Header
+          leftComponent={{
+            icon: 'menu',
+            color: '#fff',
+            onPress: () => clickHandler(),
+          }}
+          centerComponent={{text: title, style: {color: '#fff'}}}
+          rightComponent={{icon: 'home', color: '#fff'}}
         />
-      </View>
-    );
-  }
+        <View style={styles.container}></View>
+      </>
+   );
 
-  function NotificationsScreen({navigation}) {
-    return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Button onPress={() => navigation.goBack()} title="Go back home" />
-      </View>
-    );
-  }
-
-  const Drawer = createDrawerNavigator();
-
-  function App() {
-    return (
-      <NavigationContainer>
-        <Drawer.Navigator initialRouteName="Home">
-          <Drawer.Screen name="Home" component={HomeScreen} />
-          <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-        </Drawer.Navigator>
-      </NavigationContainer>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'grey',
-  },
-});
-
-export default App;
-// export default Home;
+export default Home;
